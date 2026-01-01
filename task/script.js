259a63206d44fa0b4c197a3995ff5338dd01de83
Azaan Ajmal Khan <roghani7488@gmail.com>
2026-01-01 16:22:43 +0500

    first commit

---
+++ task/script.js
@@ -0,0 +1,41 @@
+
+
+
+
+function btn1() {
+    document.getElementById("one").style.color = "blue"
+}
+
+
+function btn2() {
+    document.getElementById("two").style.color = "purple"
+} 
+
+
+function btn3() {
+    document.getElementById("three").style.color = "green"
+}
+
+
+function btn4() {
+    document.getElementById("four").style.color = "yellow"
+}
+
+
+function btn5() {
+    document.getElementById("five").style.color = "indigo"
+}
+
+
+function btn6() {
+    document.getElementById("six").style.color = "brown"
+}
+
+function reset() {
+   document.getElementById("one").style.color = "black"
+   document.getElementById("two").style.color = "black"
+   document.getElementById("three").style.color = "black"
+   document.getElementById("four").style.color = "black"
+   document.getElementById("five").style.color = "black"
+   document.getElementById("six").style.color = "black"
+}
